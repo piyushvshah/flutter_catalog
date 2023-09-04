994f9083b631fc8ead83e1c8a75e0833b609bc0c
HungryLearnerHere <shahpiyush933@gmail.com>
2023-09-04 16:56:29 +0530

    Day 13 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
@@ -8,8 +10,26 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final jsonData = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(jsonData);
+    var productData = decodedData["products"];
+    print(productData);
+  }
 
   // This widget is the root of your application.
   @override
