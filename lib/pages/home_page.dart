62d5c970420f0ef7e8b357b9de5de677dcbc573c
HungryLearnerHere <shahpiyush933@gmail.com>
2023-09-06 09:47:06 +0530

    Day 14 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'dart:convert';
 import '../widgets/drawer.dart';
-import '../widgets/item_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -36,13 +36,28 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: CatalogModel.items.length,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 6, mainAxisSpacing: 6),
           itemBuilder: (context, index) {
-            return ItemWidget(
-              items: CatalogModel.items[index],
+            final items = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                  child: Image.network(items.image),
+                  header: Container(
+                      decoration: BoxDecoration(color: Colors.deepPurple),
+                      padding: EdgeInsets.all(3),
+                      child: items.name.text.white.make()),
+                  footer: Container(
+                      padding: EdgeInsets.all(4),
+                      color: Vx.black,
+                      child: "\$${items.price}".text.white.bold.make())),
             );
           },
+          itemCount: CatalogModel.items.length,
         ),
       ),
       drawer: MyDrawer(),
